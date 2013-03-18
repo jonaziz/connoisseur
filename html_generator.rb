3e3fce754b42d9714f09a7b0940228e329bdd18c
Jon Aziz <jonaziz@gmail.com>
2013-03-18 11:02:44 -0400

    Adding html in methods to HtmlGenerator

--- html_generator.rb
+++ html_generator.rb
@@ -1,11 +1,28 @@
 class HtmlGenerator
 
   def show
-    puts "Action: show"
+    print_header
+    puts "    Action: show"
+    print_footer
   end
 
   def index
-    puts "Action: index"
+    print_header
+    puts "    Action: index"
+    print_footer
+  end
+
+  def print_header
+    puts "<html>"
+    puts "  <head>"
+    puts "    <title>Connoisseur</title>"
+    puts "  </head>"
+    puts "  <body>"
+   end
+
+  def print_footer
+    puts "  </body>"
+    puts "</html>"
   end
 
 end
